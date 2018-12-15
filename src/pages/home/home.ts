bdddc6966777d0910ed0118511b34c74c890d59c
Balaj Khan <balajkhan07@gmail.com>
2018-12-15 13:37:02 +0500

    Latest upto date

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,39 +14,38 @@ export class HomePage {
 data = {
   
   "items": [
-      {
-          "id": 1,
-          "avatar": "assets/images/avatar/16.jpg",
-          "title": "Vengeful Spirit IPA",
-          "subtitle": "Stone Brewing",
-          
-      },
-      {
-          "id": 2,
-          "avatar": "assets/images/avatar/17.jpg",
-          "title": "Logan Rogers",
-          "subtitle": "@logan",
-          
-      },
-      {
-          "id": 3,
-          "avatar": "assets/images/avatar/18.jpg",
-          "title": "Gary Russell",
-          "subtitle": "@gary",
-          
-      },
-      {
-          "id": 4,
-          "avatar": "assets/images/avatar/19.jpg",
-          "title": "Amy Lee",
-          "subtitle": "@amy",
-          "button": "Follow"
-      },
-      
-      
+    {
+        "id": 1,
+        "avatar": "assets/images/avatar/16.jpg",
+        "title": "Vengeful Spirit IPA",
+        "subtitle": "Stone Brewing",
+        
+    },
+    {
+        "id": 2,
+        "avatar": "assets/images/avatar/17.jpg",
+        "title": "Logan Rogers",
+        "subtitle": "@logan",
+        
+    },
+    {
+        "id": 3,
+        "avatar": "assets/images/avatar/18.jpg",
+        "title": "Gary Russell",
+        "subtitle": "@gary",
+        
+    },
+    {
+        "id": 4,
+        "avatar": "assets/images/avatar/19.jpg",
+        "title": "Amy Lee",
+        "subtitle": "@amy",
+        "button": "Follow"
+    },    
   ]
 };
-itemsPage(){
-this.navCtrl.push(ItemsPage);
-}
+    itemsPage(){
+        this.navCtrl.push(ItemsPage);
+    }
+
 }
